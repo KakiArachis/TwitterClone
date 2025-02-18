3a6a5eed01d4ab118bb48c35cf2cb1c233a540ea
KakiArachis <191298368+KakiArachis@users.noreply.github.com>
2025-02-18 21:01:25 +0900

    その他の画面＆共通化

--- Views/post.php
+++ Views/post.php
@@ -4,42 +4,14 @@ include_once('../config.php');
 //便利な関数を読み込む
 include_once('../util.php');
 
-////////////////////////////
-//ツイート一覧
-////////////////////////////
-$view_tweets = [
-    [
-        'user_id' => 1,
-        'user_name' => 'taro',
-        'user_nickname' => '太郎',
-        'user_image_name' => 'sample-person.jpg',
-        'tweet_body' => '今プログラミングをしています。',
-        'tweet_image_name' => 'sample-person.jpg',
-        'tweet_created_at' => '2025-01-15 14:00:00',
-        'like_id' => null,
-        'like_count' => 0,
-    ],
-    [
-        'user_id' => 2,
-        'user_name' => 'jiro',
-        'user_nickname' => '次郎',
-        'user_image_name' => null,
-        'tweet_body' => 'コワーキングスペースをオープンしました。',
-        'tweet_image_name' => 'sample-post.jpg',
-        'tweet_created_at' => '2021-03-14 14:00:00',
-        'like_id' => 1,
-        'like_count' => 1,
-    ],
-];
-
 ?>
 <!DOCTYPE html>
 <html lang="ja">
 
 <head>
     <?php include_once('../Views/common/head.php'); ?>
-    <title>ホーム画面/Twitterクローン</title>
-    <meta name="description" content="ホーム画面です">
+    <title>つぶやく画面/Twitterクローン</title>
+    <meta name="description" content="つぶやく画面です">
 </head>
 
 <body class="home">
@@ -49,7 +21,7 @@ $view_tweets = [
 
         <!-- メインの中の、ヘッダー部分作成 -->
             <div class="main-header">
-                <h1>ホーム</h1>
+                <h1>つぶやく</h1>
             </div>
 
         <!-- メインの中の、つぶやき投稿エリア作成 -->
@@ -72,16 +44,6 @@ $view_tweets = [
             
         <!-- メインの中の、仕切りエリア作成 -->
             <div class="ditch"></div>
-
-        <!-- メインの中の、つぶやき一覧エリア作成 -->
-            <?php if(empty ($view_tweets)) : ?>
-                <p class="p-3">ツイートがありません</p>
-            <?php else : ?>
-                <div class="tweet-list">
-                <?php foreach($view_tweets as $view_tweet): ?>
-                    <?php include('../Views/common/tweet.php'); ?>
-                <?php endforeach; ?>
-            <?php endif; ?>
         </div>
     </div>
     <?php include_once('../Views/common/foot.php'); ?>
--- Views/prof.php
+++ Views/prof.php
@@ -1,11 +1,12 @@
 <?php
-//設定関連を読み込む
+// 設定関連を読み込む
 include_once('../config.php');
-//便利な関数を読み込む
+// 便利な関数を読み込む
 include_once('../util.php');
-////////////////////////////
-//ツイート一覧
-////////////////////////////
+ 
+///////////////////////////////////////
+// ツイート一覧
+///////////////////////////////////////
 $view_tweets = [
     [
         'user_id' => 1,
@@ -13,38 +14,35 @@ $view_tweets = [
         'user_nickname' => '太郎',
         'user_image_name' => 'sample-person.jpg',
         'tweet_body' => '今プログラミングをしています。',
-        'tweet_image_name' => 'sample-person.jpg',
-        'tweet_created_at' => '2025-01-15 14:00:00',
+        'tweet_image_name' => null,
+        'tweet_created_at' => '2021-03-15 14:00:00',
         'like_id' => null,
         'like_count' => 0,
     ],
 ];
 ?>
-
 <!DOCTYPE html>
 <html lang="ja">
-
+ 
 <head>
     <?php include_once('../Views/common/head.php'); ?>
-    <title>プロフィール画面/Twitterクローン</title>
+    <title>プロフィール画面 / Twitterクローン</title>
     <meta name="description" content="プロフィール画面です">
 </head>
-
+ 
 <body class="home profile text-center">
     <div class="container">
-
         <?php include_once('../Views/common/side.php'); ?>
-        <div class="main"> <!-- メイン -->
-
-        <!-- メインの中の、ヘッダー部分作成 -->
+        <div class="main">
             <div class="main-header">
                 <h1>太郎</h1>
             </div>
-        <!-- プロフィールエリア -->   
+ 
+            <!-- プロフィールエリア -->
             <div class="profile-area">
                 <div class="top">
-                    <div class="user"><img src="<?php echo HOME_URL;?>Views/img_uploaded/user/sample-person.jpg" alt=""></div>
-
+                    <div class="user"><img src="<?php echo HOME_URL; ?>Views/img_uploaded/user/sample-person.jpg" alt=""></div>
+ 
                     <?php if (isset($_GET['user_id'])) : ?>
                         <!-- 相手のページ -->
                         <?php if (isset($_GET['case'])) : ?>
@@ -57,8 +55,6 @@ $view_tweets = [
                         <button class="btn btn-reverse btn-sm" data-bs-toggle="modal" data-bs-target="#js-modal">プロフィール編集</button>
                     <?php endif; ?>
  
-                    <!-- <button class="btn btn-reverse btn-sm" data-bs-toggle="modal" data-bs-target="#js-modal">プロフィール編集</button> -->
-
                     <div class="modal fade" id="js-modal" tabindex="-1" aria-hidden="true">
                         <div class="modal-dialog">
                             <div class="modal-content">
@@ -69,30 +65,33 @@ $view_tweets = [
                                     </div>
                                     <div class="modal-body">
                                         <div class="user">
-                                            <img src="<?php echo HOME_URL;?>Views/img_uploaded/user/sample-person.jpg" alt="">
+                                            <img src="<?php echo HOME_URL; ?>Views/img_uploaded/user/sample-person.jpg" alt="">
                                         </div>
                                         <div class="mb-3">
                                             <label for="" class="mb-1">プロフィール写真</label>
                                             <input type="file" class="form-control form-control-sm" name="image">
                                         </div>
+ 
                                         <input type="text" class="form-control mb-4" name="nickname" value="太郎" placeholder="ニックネーム" maxlength="50" required>
-                                        <input type="text" class="form-control mb-4" name="name" value="taro"  placeholder="ユーザー名" maxlength="50" required>
+                                        <input type="text" class="form-control mb-4" name="name" value="taro" 　placeholder="ユーザー名" maxlength="50" required>
                                         <input type="email" class="form-control mb-4" name="email" value="taro@techis.jp" placeholder="メールアドレス" maxlength="254" required>
                                         <input type="password" class="form-control mb-4" name="password" value="" placeholder="パスワードを変更する場合ご入力ください" minlength="4" maxlength="128">
                                     </div>
-
+ 
                                     <div class="modal-footer">
                                         <button class="btn btn-reverse" data-bs-dismiss="modal">キャンセル</button>
                                         <button class="btn" type="submit">保存する</button>
                                     </div>
+ 
                                 </form>
                             </div>
                         </div>
                     </div>
                 </div>
+ 
                 <div class="name">太郎</div>
                 <div class="text-muted">@taro</div>
-
+ 
                 <div class="follow-follower">
                     <div class="follow-count">1</div>
                     <div class="follow-text">フォロー中</div>
@@ -100,22 +99,23 @@ $view_tweets = [
                     <div class="follow-text">フォロワー</div>
                 </div>
             </div>
-        <!-- メインの中の、仕切りエリア作成 -->
+ 
+            <!-- 仕切りエリア -->
             <div class="ditch"></div>
-
-        <!-- メインの中の、つぶやき一覧エリア作成 -->
-        <?php if(empty ($view_tweets)) : ?>
+ 
+            <!-- つぶやき一覧エリア -->
+            <?php if (empty($view_tweets)) : ?>
                 <p class="p-3">ツイートがありません</p>
             <?php else : ?>
                 <div class="tweet-list">
-                <?php foreach($view_tweets as $view_tweet): ?>
-                    <?php include('../Views/common/tweet.php'); ?>
-                <?php endforeach; ?>
+                    <?php foreach ($view_tweets as $view_tweet) : ?>
+                        <?php include('../Views/common/tweet.php'); ?>
+                    <?php endforeach; ?>
+                </div>
             <?php endif; ?>
         </div>
     </div>
     <?php include_once('../Views/common/foot.php'); ?>
-
 </body>
-
+ 
 </html>
